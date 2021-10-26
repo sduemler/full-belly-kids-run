bcaefa40c080e8af181fc15a700b8508b900fcbf
Sam Duemler <samduemler@gmail.com>
2021-10-26 12:11:26 -0500

    Updated landing page with FYI

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -4,6 +4,10 @@ const Landing = () => {
   return (
     <div>
       <h1>This is the landing page!</h1>
+      <h2>
+        FYI for Rebecca and Bo, this is where the intro to the challenge, fun
+        run, and how to instructions will go.
+      </h2>
     </div>
   );
 };
