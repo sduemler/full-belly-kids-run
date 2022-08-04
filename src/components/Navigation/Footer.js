ccdb7c4b9c09fa1c150ee43ff650ffbd43c06b30
sduemelr <samduemler@gmail.com>
2022-08-03 21:21:09 -0400

    Updated footer and dependencies

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import enfd from '../../resources/images/ENFD_youth Challange.png';
+import academy from '../../resources/images/11_Academy_Primary_Mark_RGB.png';
 import fbgf from '../../resources/images/Full Belly Graphic White.png';
-import fbw from '../../resources/images/Full Belly web5.png';
+import fbw from '../../resources/images/Full Belly Footer 2022.jpg';
 import { Menu, Container } from 'semantic-ui-react';
 
 const Footer = () => {
@@ -9,7 +9,7 @@ const Footer = () => {
     <div>
       <Container>
         <Menu className='ui bottom fluid three stackable menu'>
-          <img className='ui centered small image' alt='enfd' src={enfd} />
+          <img className='ui centered small image' alt='enfd' src={academy} />
           <img className='ui centered large image' alt='enfd' src={fbw} />
           <img className='ui centered small image' alt='enfd' src={fbgf} />
         </Menu>
